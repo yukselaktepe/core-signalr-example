b88224ac8ac13c89034d9d62031865e925964817
Yüksel Aktepe <yukselaktepe@hotmail.com.tr>
2019-06-11 10:47:54 +0300

    Initial commit

---
+++ SignalrClient/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalrClient.Models;
+
+namespace SignalrClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public class ExamplenHub : Hub
+        {
+            public override Task OnConnectedAsync()
+            {
+                return Clients.Client(Context.ConnectionId).SendAsync("SetConnectionId", Context.ConnectionId);
+            }
+            public async Task<string> ConnectGroup(string stocName, string connectionID)
+            {
+                await Groups.AddToGroupAsync(connectionID, stocName);
+                return $"{connectionID} is added {stocName}";
+            }
+            public Task PushNotify(string text)
+            {
+                return Clients.Group("ExampleGroup").SendAsync("ChangeProductValue", text);
+            }
+        }
+    }
+}
---
+++ SignalrService/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalrService
+{
+    class Program
+    {
+        static HubConnection connectionSignalR;
+        static void Main(string[] args)
+        {
+            Connect().Wait();
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("> value<enter>");
+
+            var cancelled = false;
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cancelled = true;
+            };
+
+            while (!cancelled)
+            {
+                Console.Write("> ");
+
+                string text;
+                try
+                {
+                    text = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+                if (text == null)
+                {
+                    break;
+                }
+
+                try
+                {
+                    connectionSignalR.InvokeAsync("PushNotify", text);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"failed to deliver message: {e.Message}");
+                }
+            }
+        }
+
+        public static async Task Connect()
+        {
+            connectionSignalR = new HubConnectionBuilder()
+               .WithUrl("http://localhost:5555/ExampleHub")
+               .Build();
+            await connectionSignalR.StartAsync();
+        }
+    }
+}
